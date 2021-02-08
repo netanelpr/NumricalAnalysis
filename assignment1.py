5a795b9a627e3b50dd506e317bbd7c3e417e5968
netanelpr <pratn@post.bgu.ac.il>
2021-02-08 18:40:01 +0200

    Implement part1 for above three points

--- assignment1.py
+++ assignment1.py
@@ -51,10 +51,49 @@ class Assignment1:
         The interpolating function.
         """
 
-        # replace this line with your solution to pass the second test
-        result = f
+        def find_bezier_cubic_control_point(number, p0, p2):
+            return (number - (p0 * 0.25 + p2 * 0.25)) * 2
 
-        return result
+        def cubic_bezier(p0, p1, p2, t):
+            #bezier_function = lambda p0, p1, p2, t: p0 * (1 - t) ** 2 + 2 * p1 * t * (1 - t) + p2 * t ** 2
+            #return [bezier_function(p0[0], p1[0], p2[0], t), bezier_function(p0[1], p1[1], p2[1], t)]
+            return p0 * (1 - t) ** 2 + 2 * p1 * t * (1 - t) + p2 * t ** 2
+
+        def interpolated_func_data(points, control_points):
+            def interpolated_function(x):
+                current_point_x = points[0][0]
+                print(current_point_x)
+                index = 0
+                while(x < current_point_x and index < len(points)-1):
+                    current_point_x = points[i][0]
+                    index = index + 1
+                print(index)
+                p0 = points[index]
+                p1 = control_points[index]
+                p2 = points[index+1]
+
+                t = (x - p0[0]) / (p2[0] - p0[0])
+                return cubic_bezier(p0[1], p1[1], p2[1], t)
+
+            return interpolated_function
+                
+        
+        jmp = (b - a) / (n-2)
+        #points.append((a + a + jmp) / 2)
+        points = [[a, f(a)]] + [[a + jmp*i, f(a + jmp*i)] for i in range(1, n-1)]
+       
+        print(points)
+        cp_x = find_bezier_cubic_control_point((a + a + jmp) / 2, points[0][0], points[1][0])
+        cp_y = find_bezier_cubic_control_point(f((a + a + jmp) / 2), points[0][1], points[1][1])
+        control_points = [[cp_x, cp_y]]
+ 
+        for i in range(1, n-2):
+            cp_x = 2 * points[i][0] - cp_x
+            cp_y = 2 * points[i][1] - cp_y
+            control_points.append([cp_x, cp_y])
+        print(f"control points {control_points}")
+
+        return interpolated_func_data(points, control_points)
 
 
 ##########################################################################
@@ -96,6 +135,12 @@ class TestAssignment1(unittest.TestCase):
         print(T)
         print(mean_err)
 
+    def t_1(self):
+        assignment1 = Assignment1()
+        f1 = lambda x: x ** 2
+        interpolated = assignment1.interpolate(f1, -1, 1, 4)
+        self.assertEqual(1.0, interpolated(1))
+
     def test_with_poly_restrict(self):
         ass1 = Assignment1()
         a = np.random.randn(5)
@@ -108,3 +153,5 @@ class TestAssignment1(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+
