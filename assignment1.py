13f70e4090c733d886e5b597c1d44206f4c22f27
netanelpr <pratn@post.bgu.ac.il>
2021-02-09 08:40:43 +0200

    Update part 1

--- assignment1.py
+++ assignment1.py
@@ -60,16 +60,16 @@ class Assignment1:
             return p0 * (1 - t) ** 2 + 2 * p1 * t * (1 - t) + p2 * t ** 2
 
         def interpolated_func_data(points, control_points):
-            def interpolated_function(x):
+            def bezier(x):
                 current_point_x = points[0][0]
                 #print(current_point_x)
                 index = 0
                 while(x > current_point_x and index < len(points)-1):
-                    current_point_x = points[index][0]
+                    #print(f"{x} {current_point_x}")
                     index = index + 1
+                    current_point_x = points[index][0]
 
-                if(index == len(points)-1):
-                    index = index - 1
+                index = index - 1
                 #print(len(points))
                 #print(len(control_points))
                 #print(f"index {index}")
@@ -84,26 +84,121 @@ class Assignment1:
                 #print(f"t {t} x {x}")
                 return cubic_bezier(p0[1], p1[1], p2[1], t)
 
-            return interpolated_function
-                
+            def lagrange3(x):
+                def li_i_2(x, p1_x, p2_x, p3_x):
+                    return (x - p2_x) / (p1_x - p2_x) * (x - p3_x) / (p1_x - p3_x)
+
+                current_point_x = points[0][0]
+                #print(current_point_x)
+                index = 0
+                while(x > current_point_x and index < len(points)-1):
+                    #print(f"{x} {current_point_x}")
+                    index = index + 1
+                    current_point_x = points[index][0]
+
+                index = index - 1
+                #print(x)
+                #print(len(points))
+                #print(len(control_points))
+                #print(f"index {index}")
+
+                c_points = [points[index], control_points[index], points[index+1]]
+                #print(c_points)
+                y_x = 0
+                for i in range(3):
+                    p1 = c_points[i-3]
+                    p2 = c_points[i-2]
+                    p3 = c_points[i-1]
+                    y_x = y_x + p1[1] * li_i_2(x, p1[0], p2[0], p3[0])
+
+                return y_x
+
+            def lagrange2(x):
+                def li_i_1(x, p1_x, p2_x):
+                    return (x - p2_x) / (p1_x - p2_x)
+
+                current_point_x = points[0][0]
+                #print(current_point_x)
+                index = 0
+                while(x > current_point_x and index < len(points)-1):
+                    #print(f"{x} {current_point_x}")
+                    index = index + 1
+                    current_point_x = points[index][0]
+
+                index = index - 1
+                #print(x)
+                #print(len(points))
+                #print(len(control_points))
+                #print(f"index {index}")
+                c_points = [points[index], points[index+1]]
+                #print(c_points)
+                y_x = 0
+                for i in range(2):
+                    p1 = c_points[i-2]
+                    p2 = c_points[i-1]
+                    y_x = y_x + p1[1] * li_i_1(x, p1[0], p2[0])
+
+                return y_x
+
+            return lagrange2
         
-        jmp = (b - a) / (n-2)
-        points = [[a, f(a)]] + [[a + jmp*i, f(a + jmp*i)] for i in range(1, n-1)]
+        
+
+        def one_control_point():
+            jmp = (b - a) / (n-2)
+            points = [[a, f(a)]] + [[a + jmp*i, f(a + jmp*i)] for i in range(1, n-1)]
        
-        #print(points)
-        #print(len(points))
-        cp_x = find_bezier_cubic_control_point((a + a + jmp) / 2, points[0][0], points[1][0])
-        cp_y = find_bezier_cubic_control_point(f((a + a + jmp) / 2), points[0][1], points[1][1])
-        control_points = [[cp_x, cp_y]]
+            #print(points)
+            #print(len(points))
+            cp_x = find_bezier_cubic_control_point((a + a + jmp) / 2, points[0][0], points[1][0])
+            cp_y = find_bezier_cubic_control_point(f((a + a + jmp) / 2), points[0][1], points[1][1])
+            control_points = [[cp_x, cp_y]]
  
-        for i in range(1, n-2):
-            cp_x = 2 * points[i][0] - cp_x
-            cp_y = 2 * points[i][1] - cp_y
-            control_points.append([cp_x, cp_y])
-        #print(f"\n\ncontrol points {control_points} len {len(control_points)}\n\n")
+            for i in range(1, n-2):
+                cp_x = 2 * points[i][0] - cp_x
+                cp_y = 2 * points[i][1] - cp_y
+                control_points.append([cp_x, cp_y])
+            #print(f"\n\ncontrol points {control_points} len {len(control_points)}\n\n")
+            return (points, control_points)
 
+        def half_control_point():
+            number_of_points = int((n/2)) - 1
+            jmp = (b - a) / number_of_points
+            points = [[a, f(a)]]
+            control_points = []
+            point_x = a + jmp
+            for i in range(1, number_of_points+1):
+                points.append([point_x, f(point_x)])
+                point_x = point_x + jmp
+
+            for i in range(0, number_of_points):
+                p1_x = points[i][0]
+                p1_y = points[i][1]
+                p2_x = points[i+1][0]
+                p2_y = points[i+1][1]
+                cp_x = find_bezier_cubic_control_point((p2_x + p1_x) / 2, p1_x, p2_x)
+                cp_y = find_bezier_cubic_control_point(f((p2_y + p1_y) / 2), p1_y, p2_y)
+                control_points.append([cp_x, cp_y]) 
+
+            #print(f"points {points} len {len(points)}")
+            #print(f"control points {control_points} len {len(control_points)}")
+            return (points, control_points)
+ 
+        def all_points():
+            number_of_points = n
+            jmp = (b - a) / number_of_points
+            points = []
+            point_x = a
+            for i in range(0, number_of_points):
+                points.append([point_x, f(point_x)])
+                point_x = point_x + jmp
+
+            #print(f"points {points} len {len(points)}")
+            #print(f"control points {control_points} len {len(control_points)}")
+            return (points, [])
+
+        points, control_points = all_points()
         return interpolated_func_data(points, control_points)
-
 
 ##########################################################################
 
@@ -128,7 +223,16 @@ class TestAssignment1(unittest.TestCase):
             f = np.poly1d(a)
 
             ff = ass1.interpolate(f, -10, 10, 300 + 1)
-            xs = (np.random.random(200) -0.5) *  10
+            xs1 = []
+            if(-10 < 0):
+                for number in np.random.random(200):
+                    if (number < 0.5):
+                        xs1.append(-1)
+                    else:
+                        xs1.append(1)
+            else:
+                xs1 = 1
+            xs = (np.random.random(200))* xs1 * ((-10 - 10) / 2)
             err = 0
             for x in xs:
                 #print(x)
